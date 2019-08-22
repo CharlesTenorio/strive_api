4fd69e353612bb4a5b5276197e4601e09147b12b
CharlesTenorio <charlestenorios@gmail.com>
2019-08-22 20:18:13 -0300

    teste

--- cielo/tests.py
+++ cielo/tests.py
@@ -4,22 +4,6 @@ from cielo.api.funcao import comprar_credito
 
 
 class TestCielo(TestCase):
-    def test_merchant_id(self):
-        mid = config('MERCHANT_ID')
-        assert mid == '4dfff8d6-4e18-4d7d-af75-8c9345b0c5cb'
-
-    def test_merchant_key(self):
-        mid = config('MERCHANT_KEY')
-        assert mid == 'K8nfYeNg1CtSyKN8DO4SfSyx37Wq0JAyUpFF2phL'
-
-    def test_merchant_id_box(self):
-        mid = config('MERCHANT_ID_BOX')
-        assert mid == 'bde2dd94-207b-4303-8939-11f18389a67f'
-
-    def test_merchant_key_box(self):
-        mid = config('MERCHANT_KEY_BOX')
-        assert mid == 'SGZZBNYUQFFBCCXABXJRDSSEDCPAWBYXCZGYQSMF'
-
     def test_url_producao_post(self):
         url = config('CIELO_PRD_PUT_POST')
         assert url == 'https://api.cieloecommerce.cielo.com.br/'
