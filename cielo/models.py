91b5f653dd6a1f924ea9fa1e15fb44421808a700
CharlesTenorio <charlestenorios@gmail.com>
2019-08-22 21:02:44 -0300

    model para pegar trascao

---
+++ cielo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class ComprarCredito(models.Model):
+    valor = models.DecimalField(max_digits=11, decimal_places=3)
+    qtd_parcela = models.PositiveIntegerField(default=1, blank=True, null=True)
+    codigo_trasacao = models.CharField(max_length=80, default='0', blank=True, null=True)
+    statu_trasacao = models.CharField(max_length=300, blank=True, null=True)
+    data_compra = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-data_compra',)
