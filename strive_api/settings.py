d95a9fbbc68e5c34245617f6d7efd16678635052
CharlesTenorio <charlestenorios@gmail.com>
2019-08-28 09:48:14 -0300

    novo id aws

--- strive_api/settings.py
+++ strive_api/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '18.224.8.207']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.16.200.200']
 # Application definition
 
 INSTALLED_APPS = [
