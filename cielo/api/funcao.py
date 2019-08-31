dc3961157bb9c76a7b0d917eb2ebcd1ef7d4309a
CharlesTenorio <charlestenorios@gmail.com>
2019-08-31 11:05:28 -0300

    usando Lib Cielo gbrennon.cieloApi3

--- cielo/api/funcao.py
+++ cielo/api/funcao.py
@@ -1,7 +1,13 @@
-from decouple import config
-import requests
 import json
 import logging
+from cieloApi3 import Environment
+from cieloApi3 import Merchant
+from cieloApi3 import Sale
+from cieloApi3 import Customer
+from cieloApi3 import CreditCard
+from cieloApi3 import CieloEcommerce
+from cieloApi3 import Payment
+from decouple import config
 
 
 loogger = logging.getLogger('erro_cielo')
@@ -11,50 +17,36 @@ key_cliente = config('MERCHANT_KEY')
 id_cliente_hoodid = config('MERCHANT_ID_HOODID')
 key_cliente_hoodid = config('MERCHANT_KEY_HODID')
 
+environment = Environment(sandbox=False)
+
+merchant = Merchant(id_cliente, key_cliente)
+
 
 def comprar_credito(id_compra, cliente, numero_cartao, seguranca, bandeira, validade, valor, qtd_parcela):
+    msg_retorno = ''
+    codigo_transacao = ''
+    x = ''
+
     try:
-        headers = {'Content-Type': 'application/json', 'MerchantId': id_cliente, 'MerchantKey': key_cliente}
-        data = {
-            "MerchantOrderId": id_compra,
-            "Customer": {"Name": cliente},
-            "Payment": {"Type": "CreditCard", "Amount": valor, "Installments": qtd_parcela,
-                        "SoftDescriptor": "123456789ABCD",
-                        "CreditCard": {"CardNumber": numero_cartao, "Holder": cliente,
-                                       "ExpirationDate": validade, "SecurityCode": seguranca, "Brand": bandeira},
-                        "IsCryptoCurrencyNegotiation": True
-                        }
-        }
+        sale = Sale(id_compra)
+        sale.customer = Customer(cliente)
+        credit_card = CreditCard(seguranca, bandeira)
+        credit_card.expiration_date = validade
+        credit_card.security_code = seguranca
+        credit_card.card_number = numero_cartao
+        credit_card.holder = cliente
+        sale.payment = Payment(valor)
+        sale.payment.credit_card = credit_card
+        sale.payment.installments = qtd_parcela
+        cielo_ecommerce = CieloEcommerce(merchant, environment)
+        response_create_sale = cielo_ecommerce.create_sale(sale)
+        x = json.dumps(response_create_sale, indent=3, sort_keys=True)
+        # payment_id = sale.payment.payment_id
+        resultado = json.loads(x)
+        codigo_transacao = resultado["Payment"]["Tid"]
+        msg_retorno = resultado["Payment"]["ReturnMessage"]
 
-        r = requests.post(url_prd + '1/sales/', data=json.dumps(data), headers=headers)
-        resposa = r.json()
-        codigo_transacao = resposa["Payment"]["Tid"]
-        msg_retorno = resposa["Payment"]["ReturnMessage"]
-
-        return msg_retorno, codigo_transacao
-    except Exception as e:
+    except KeyError as e:
         print(e)
 
-
-def cartao_hoodid(id_compra, cliente, numero_cartao, seguranca, bandeira, validade, valor, qtd_parcela):
-    try:
-        headers = {'Content-Type': 'application/json', 'MerchantId': id_cliente_hoodid, 'MerchantKey': key_cliente_hoodid}
-        data = {
-            "MerchantOrderId": id_compra,
-            "Customer": {"Name": cliente},
-            "Payment": {"Type": "CreditCard", "Amount": valor, "Installments": qtd_parcela,
-                        "SoftDescriptor": "123456789ABCD",
-                        "CreditCard": {"CardNumber": numero_cartao, "Holder": cliente,
-                                       "ExpirationDate": validade, "SecurityCode": seguranca, "Brand": bandeira},
-                        "IsCryptoCurrencyNegotiation": True
-                        }
-        }
-
-        r = requests.post(url_prd + '1/sales/', data=json.dumps(data), headers=headers)
-        resposa = r.json()
-        codigo_transacao = resposa["Payment"]["Tid"]
-        msg_retorno = resposa["Payment"]["ReturnMessage"]
-
-        return msg_retorno, codigo_transacao
-    except Exception as e:
-        print(e)
+    return (msg_retorno, codigo_transacao)
